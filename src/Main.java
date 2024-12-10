aa36bdf61ae32465d5db122bb460d23b93253b9d
Beliypozhizni <beliypozhizni@gmail.com>
2024-12-10 21:42:55 +0300

    The task is completed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic(10, 5);
+
+        System.out.printf("Sum: %d\n", arithmetic.sum());
+        System.out.printf("Multiply: %d\n", arithmetic.multiply());
+        System.out.printf("Max: %d\n", arithmetic.max());
+        System.out.printf("Min: %d\n", arithmetic.min());
+    }
+}
